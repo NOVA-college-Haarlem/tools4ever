b2bc9935ee0dde5967f924450774903788073a25
David van Oudheusden <40887850+MeesterDaaf@users.noreply.github.com>
2024-10-01 10:23:37 +0200

    Create categories.sql

---
+++ sql/categories.sql
@@ -0,0 +1,16 @@
+-- Onderstaande code gaan we ook gebruiken maar op een later moment
+CREATE TABLE categories (
+    category_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (category_id)
+);
+
+INSERT INTO categories(name) VALUES 
+('Handgereedschap'),
+('Elektrisch gereedschap'),
+('Tegelgereedschap'),
+('Meetgereedschap'),
+('Kitgereedschap'),
+('Lijmgereedschap'),
+('Stofafzuiging'),
+('Verfgereedschap');
